7e781bb256ed280223691a70246da7ff6239a066
Z0KRI <david1166@live.com.mx>
2024-12-05 02:21:27 -0600

    ⚡️ Feat: Inject Providers

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 import App from "./App.vue";
 import router from "./common/router/main";
+import { GenerateFactoryProvider } from "./data/common/providers/generate-factory.provider";
+import { DataModule } from "./data/data.module";
 
 const app = createApp(App);
 
 app.use(router);
+
+GenerateFactoryProvider(app, DataModule);
 
 app.mount("#app");
